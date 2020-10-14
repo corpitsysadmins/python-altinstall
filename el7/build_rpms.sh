8173d651aa1a357ffee335373a5c6b6d1bff92f1
Irving Leonard <irving.leonard@modmed.com>
2020-10-13 22:43:27 -0400

    feat(version): new version 3.9

--- el7/build_rpms.sh
+++ el7/build_rpms.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]; then
+  echo 1>&2 "Usage: $0 PYTHON_SPEC_VERSION"
+  exit 3
+fi
+
 yum -y install rpmdevtools yum-utils
 
 rpmdev-setuptree
-mv python38-altinstall.spec rpmbuild/SPECS/
 
-yum-builddep -y rpmbuild/SPECS/python38-altinstall.spec
-spectool -g -R rpmbuild/SPECS/python38-altinstall.spec
-rpmbuild -ba rpmbuild/SPECS/python38-altinstall.spec
+for spec_files in *.spec; do
+	
+	if [ -e "$spec_files" ]; then
+		mv *.spec rpmbuild/SPECS/
+	else
+		echo "No new specs files were found"
+	fi
+    
+    break
+done
+
+yum-builddep -y rpmbuild/SPECS/python$1-altinstall.spec
+spectool -g -R rpmbuild/SPECS/python$1-altinstall.spec
+rpmbuild -ba rpmbuild/SPECS/python$1-altinstall.spec
